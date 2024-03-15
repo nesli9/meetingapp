a48b131ea6b5ef99278e5187c91bafc3b38dfcec
Neslihan Doğan <74837454+nesli9@users.noreply.github.com>
2024-03-15 12:06:38 +0300

    kayıt formu

--- Contollers/HomeController.cs
+++ Contollers/HomeController.cs
@@ -9,19 +9,19 @@ namespace MeetingApp.Controllers
         {
             int saat = DateTime.Now.Hour;
 
-            // ViewBag.Selamlama = saat > 12 ? "İyi Günler":"Günaydın";
-            // ViewBag.UserName = "Çınar";
+            //ViewData["Selamlama"] = saat > 12 ? "İyi Günler":"Günaydın";
+            //ViewData["UserName"] = "Çınar";
 
             ViewData["Selamlama"] = saat > 12 ? "İyi Günler":"Günaydın";
             ViewData["UserName"] = "Çınar";
 
-            var meetingInfo = new MeetingInfo(){
+            var meetingInfo = new MeetingInfo() 
+            {
                 Id = 1,
-                Location = "İstanbul , ABC Kongre mergezi",
-                Date = new DateTime(2024 , 01,20,20,0,0),
-                NumberOfPeople = 100;
- 
-            }; 
+                Location = "İstanbul, Abc Kongre Merkezi",
+                Date = new DateTime(2024, 01, 20, 20, 0, 0),
+                NumberOfPeople = 100
+            };
 
             return View(meetingInfo);
         }
