5297d0828abae6a7eb695a2eed7092ba382e1f46
Neslihan Doğan <74837454+nesli9@users.noreply.github.com>
2024-03-15 13:00:54 +0300

    HTTP Verbs

--- Contollers/MeetingController.cs
+++ Contollers/MeetingController.cs
@@ -3,12 +3,17 @@ using Microsoft.AspNetCore.Mvc;
 namespace MeetingApp.Contollers{
     public class MeetingController : Controller{
 
+        [HttpGet]
         public IActionResult Index(){
             return View();
         }
+        [HttpGet]
+
         public IActionResult Apply(){
             return View();
         }
+        [HttpGet]
+
         public IActionResult List(){
             return View();
         }
