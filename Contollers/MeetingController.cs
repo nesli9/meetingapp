fa679a0c49db43d736b558efccb8e93f0700072d
Neslihan Doğan <74837454+nesli9@users.noreply.github.com>
2024-03-17 11:48:52 +0300

    tag helpers

--- Contollers/MeetingController.cs
+++ Contollers/MeetingController.cs
@@ -1,3 +1,4 @@
+using MeetingApp.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MeetingApp.Contollers{
@@ -14,7 +15,7 @@ namespace MeetingApp.Contollers{
         }
         [HttpPost]
 
-        public IActionResult Apply(string Name , string Phone , string Email , bool WillAttend){
+        public IActionResult Apply(UserInfo model){
             
             return View();
         }
